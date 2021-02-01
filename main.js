4b374fda64a133950cdd012ac1e0bde12a1de595
Sam Dunegan <sam.dunegan@sbcglobal.net>
2021-02-01 13:31:39 -0600

    new

--- main.js
+++ main.js
@@ -33,22 +33,58 @@ const printBoard = () => {
 }
 
 const horizontalWin = () => {
+if(
+  (board[0][0]== 'O' && board[0][1] == 'O' && board [0][2] == ' O') ||
+  (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O') ||
+  (board[1][0] == 'O' && board[1][2] == 'O' && board[1][1] == 'O') ||
+  (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') ||
+  (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') ||
+  (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X')
+) {
+  return true;
+}
   // Your code here to check for horizontal wins
 }
 
 const verticalWin = () => {
+  if(
+    (board[0][1]== 'O' && board[1][1] == 'O' && board [2][1] == ' O') ||
+    (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O') ||
+    (board[1][2] == 'O' && board[0][2] == 'O' && board[2][2] == 'O') ||
+    (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') ||
+    (board[1][1] == 'X' && board[0][1] == 'X' && board[2][1] == 'X') ||
+    (board[1][0] == 'X' && board[2][0] == 'X' && board[0][0] == 'X')
+  ) {
+    return true;
+  }
   // Your code here to check for vertical wins
 }
 
 const diagonalWin = () => {
+
+  if (
+    (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') ||
+    (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O') ||
+    (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') ||
+    (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X')
+  ) {
+    return true
+  }
   // Your code here to check for diagonal wins
 }
 
 const checkForWin = () => {
+  if ( horizontalWin() || verticalWin() || diagonalWin ())
+  {
+    return "you have won"
+  }
   // Your code here call each of the check for types of wins
 }
 
 const ticTacToe = (row, column) => {
+  if (checkForWin()){
+    console.log('you have won');
+  }
   // Your code here to place a marker on the board
   // then check for a win
 }
